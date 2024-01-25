30e87246dfe12c8ce523225942a54967f024f834
Nicolas Boskovic <nboskovic@gmail.com>
2024-01-25 11:30:44 -0300

    Fix

--- miners/bitcoin/funds_flow/version-script.sh
+++ miners/bitcoin/funds_flow/version-script.sh
@@ -13,23 +13,22 @@ fi
 DIGEST=$(docker image inspect --format='{{index .RepoDigests 0}}' "ghcr.io/blockchain-insights/blockchain_insights_base")
 
 SHA256_DIGEST=$(echo "$DIGEST" | cut -d "@" -f2)
-VERSION=${SHA256_DIGEST:7:4}${SHA256_DIGEST: -4}
-echo "Version: $VERSION"
+DIGEST_CHARS=${SHA256_DIGEST:7:4}${SHA256_DIGEST: -4}
 
 # Path to the .env file
 ENV_FILE=".env"
 
 # Check if .env file exists
 if [ -f "$ENV_FILE" ]; then
-    # Check if VERSION variable exists in the file
-    if grep -q "VERSION=" "$ENV_FILE"; then
-        # Replace the existing VERSION value
-        sed -i "s/^VERSION=.*/VERSION=$VERSION/" "$ENV_FILE"
+    # Check if DIGEST_CHARS variable exists in the file
+    if grep -q "DIGEST_CHARS=" "$ENV_FILE"; then
+        # Replace the existing DIGEST_CHARS value
+        sed -i "s/^DIGEST_CHARS=.*/DIGEST_CHARS=$DIGEST_CHARS/" "$ENV_FILE"
     else
-        # Add VERSION variable to the file
-        echo "VERSION=$VERSION" >> "$ENV_FILE"
+        # Add DIGEST_CHARS variable to the file
+        echo "DIGEST_CHARS=$DIGEST_CHARS" >> "$ENV_FILE"
     fi
 else
-    # Create .env file and add VERSION variable
-    echo "VERSION=$VERSION" > "$ENV_FILE"
+    # Create .env file and add DIGEST_CHARS variable
+    echo "DIGEST_CHARS=$DIGEST_CHARS" > "$ENV_FILE"
 fi
